e61d1e7aff0ac46c8a1ab0b37e01612b854ec006
Lory16 <lorenzo.veronese2000@gmail.com>
2021-04-04 12:23:03 +0200

    Freecodecamp course

--- Various/freecodecamp_course.py
+++ Various/freecodecamp_course.py
@@ -44,6 +44,7 @@ cv.destroyAllWindows()
 '''
 
 
+'''
 # DRAW ON PICTURE
 # initialize a blank picture
 blank = np.zeros((500, 500, 3), dtype = 'uint8') # (height, width, number of color channels), type
@@ -68,5 +69,84 @@ cv.line(blank, (0, 0), (250, 250), (255, 255, 255), thickness=2)
 cv.putText(blank, 'Hello', (255, 255), cv.FONT_HERSHEY_PLAIN, 1.0, (255, 255, 255), 2)
 cv.imshow('Blank', blank)
 cv.waitKey(0)
+'''
 
 
+'''
+# BASIC FUNCTIONS
+# converting to grayscale
+img = cv.imread('Template.jpg')
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+# blur an image (eliminate some noise)
+blur = cv.GaussianBlur(img, (3, 3), cv.BORDER_DEFAULT)
+
+# find edges
+canny = cv.Canny(blur, 125, 175) # I've passed blur!
+
+# dilating an image
+dilated = cv.dilate(canny, (3, 3), iterations = 1)
+
+# eroding
+eroded = cv.erode(dilated, (3, 3), iterations = 1)
+
+# resize
+resized = cv.resize(img, (500, 500), interpolation = cv.INTER_AREA) # INTER_CUBIC is the slowest
+
+# crop
+cropped = img[50:200, 200:400]
+cv.imshow('Test', cropped)
+cv.waitKey(0)
+'''
+
+
+'''
+# TRANSFORMATIONS
+# translation
+def translate(img, x, y):
+    transMat = np.float32([[1, 0, x], [0, 1, y]])
+    dimensions = (img.shape[1], img.shape[0])
+    return cv.warpAffine(img, transMat, dimensions)
+
+img = cv.imread('Template.jpg')
+translated = translate(img, 100, 100)
+
+# rotation
+def rotate(img, angle, rotPoint = None):
+    (height, width) = img.shape[:2]
+
+    if rotPoint is None:
+        rotPoint = (width // 2, height // 2)
+
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    dimensions = (width, height)
+
+    return cv.warpAffine(img, rotMat, dimensions)
+
+img = cv.imread('Template.jpg')
+rotated = rotate(img, 45)
+rotated_rotated = rotate(rotated, 45)
+
+# resizing 
+resized = cv.resize(img, (500, 500), interpolation = cv.INTER_CUBIC) #interpolation depends on what ypu do 
+
+# flipping
+flip = cv.flip(img, -1) #0 = flip vertically, 1 = orizzontally, -1 = both
+
+# cropping
+cropped = img[200:400, 300:400]
+cv.imshow('Test', cropped)
+cv.waitKey(0)
+'''
+
+
+
+# CONTOUR DETECTION
+img = cv.imread('Template.jpg')
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# Second and third arguments of cv2.canny are our minVal and maxVal respectively.
+canny = cv.Canny(img, 125, 175)
+
+cv.imshow('Test', canny)
+cv.waitKey(0)
