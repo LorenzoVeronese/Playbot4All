cf95bd6631d776edd4ac0240e4789ebb651bf8ab
Lory16 <lorenzo.veronese2000@gmail.com>
2021-04-04 22:14:58 +0200

    Freecodecamp course

--- Various/actual_position_detection.py
+++
@@ -1,9 +0,0 @@
-"""
-IDEA:
-    find stroke written with Canny algorithm
-    find hand
-    find the point of the stroke nearest to the hand
-    use the fact that you know where the laser is pointing
-BUT:
-    what about points near the hand but in the wrong place?
-"""
--- Various/face_detect.py
+++ Various/face_detect.py
@@ -1,12 +1,16 @@
 import cv2 as cv
 
 img = cv.imread('Template.jpg')
-cv.imshow('Person', img)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
+# very sensitive to noise
 haar_cascade = cv.CascadeClassifier('haar_face.xml')
 faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = 3)
 print(f'Number of faces cound = {len(faces_rect)}')
+
+# to display a rectangle around the face dected
+for (x, y, w, h) in faces_rect:
+    cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), thickness = 2)
 
 cv.waitKey(0)
